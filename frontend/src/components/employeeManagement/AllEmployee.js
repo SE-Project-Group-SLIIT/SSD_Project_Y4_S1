12a0187e3c8f18fafe4f469d1afdfb4596024840
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2023-10-01 23:37:43 +0530

    change all employee form routes

--- frontend/src/components/employeeManagement/AllEmployee.js
+++ frontend/src/components/employeeManagement/AllEmployee.js
@@ -1,242 +1,405 @@
-import axios from 'axios'
+import axios from "axios";
 import React, { useState, useEffect } from "react";
-import { withRouter } from 'react-router-dom';
-import { Modal, Button } from 'react-bootstrap';
-import Swal from 'sweetalert2'
-import { viewEmployees } from "../../services/util/employee"
-import UpdateEmployee from './UpdateEmployee';
-
+import { withRouter } from "react-router-dom";
+import { Modal, Button } from "react-bootstrap";
+import Swal from "sweetalert2";
+import {
+	viewEmployees,
+	inactiveEmployee,
+    activeEmployee,
+} from "../../services/util/employee";
+import UpdateEmployee from "./UpdateEmployee";
 import Header from "../shared/Header";
 
-
-
 export default function AllEmployee() {
+	// const [search, setSearch] = useState("");
 
+	const [employees, setEmployees] = useState([]);
 
-    // const [search, setSearch] = useState("");
+	const [ModalEmpUpdate, setModalEmpUpdate] = useState([]);
+	const [ModalEmpUpdateConfirm, setModalEmpUpdateConfirm] =
+		useState(false);
 
-    const [employees, setEmployees] = useState([]);
-    
-    const [ModalEmpUpdate, setModalEmpUpdate] = useState([]);
-    const [ModalEmpUpdateConfirm, setModalEmpUpdateConfirm] = useState(false);
+	const [ModalEmpDelete, setModalEmpDelete] = useState([]);
+	const [ModalEmpDeleteConfirm, setModalEmpDeleteConfirm] =
+		useState(false);
 
-    const [ModalEmpDelete, setModalEmpDelete] = useState([]);
-    const [ModalEmpDeleteConfirm, setModalEmpDeleteConfirm] = useState(false);
+        const [ModalEmpActive, setModalEmpActive] = useState([]);
+        const [ModalEmpActiveConfirm, setModalEmpActiveConfirm] =
+            useState(false);
 
+	useEffect(() => {
+		async function getEmployees() {
+			try {
+				let respond = await viewEmployees();
+				if (respond.data) {
+					setEmployees(respond.data);
+				} else {
+					console.log("error");
+				}
+			} catch (error) {
+				console.log(error);
+			}
+		}
 
-    useEffect(() => {
+		getEmployees();
+	}, []);
 
-        async function getEmployees() {
-            try{
-                let respond = await viewEmployees();
-                if(respond.data){
-                    setEmployees(respond.data);
-                }
-                else{
-                    console.log("error")
-                }
-                // axios.get("http://localhost:8070/employee/allEmp").then((res) => {
-    
-    
-                //     setEmployees(res.data.reverse());
-                //     //console.log("Data recieved");
-    
-                // }).catch((error) => {
-                //     // alert(error.message);
-                //     console.log("f354754",error);
-                //     Swal.fire({
-                //         icon: 'error',
-                //         title: 'Oops...',
-                //         text: 'Something went wrong!',
-                //         confirmButtonColor: '#207159',
-    
-                //     })
-                // })
-            }
-            catch(error){
-                console.log(error)
-            }
-            
+	const deleteEmployee = async (id) => {
+		try {
+			const response = await inactiveEmployee(id);
+			// You can handle the response as needed
+			if (response.status === 200) {
+				// Employee was made inactive successfully
+				return response.data;
+			} else {
+				// Handle the error case
+				throw new Error("Failed to make the employee inactive");
+			}
+		} catch (error) {
+			// Handle any other errors
+			console.error("An error occurred:", error);
+			throw error;
+		}
+		// console.log("deleteEmployee...", data);
+		// await axios
+		// 	.post("http://localhost:8070/REmployee/addRemovedEmp/", {
+		// 		data,
+		// 	})
+		// 	.then(() => {
+		// 		Swal.fire({
+		// 			title: "Success!",
+		// 			text: "Permenantly deleted the Employee Record",
+		// 			icon: "success",
+		// 			showConfirmButton: false,
+		// 			timer: 1500,
+		// 		});
+		// 		console.log("Emp delete modal....");
 
-        }
+		// 		console.log(ModalEmpDelete);
+		// 		const value = axios.post(
+		// 			"http://localhost:8070/employee/deleteEmp",
+		// 			ModalEmpDelete,
+		// 		);
+		// 		if (value) {
+		// 			Swal.fire({
+		// 				title: "Success!",
+		// 				text: "Permenantly deleted the Employee Record &  added successfully into the Removed Employee List !!",
+		// 				icon: "success",
+		// 				showConfirmButton: false,
+		// 				timer: 2000,
+		// 			}).then(() => {
+		// 				console.log("111111111111");
+		// 				window.location.reload();
+		// 			});
+		// 		}
+		// 	})
+		// 	.catch((err) => {
+		// 		Swal.fire({
+		// 			icon: "error",
+		// 			title: "Oops...",
+		// 			text: "Something went wrong!",
+		// 			confirmButtonColor: "#207159",
+		// 		}).then(() => {
+		// 			window.location.reload();
+		// 		});
+		// 		alert(err.response.data.errorCode);
+		// 	});
+	};
 
-        getEmployees();
+    const restoreEmployee = async (id) => {
+		try {
+			const response = await activeEmployee(id);
+			// You can handle the response as needed
+			if (response.status === 200) {
+				// Employee was made inactive successfully
+				return response.data;
+			} else {
+				// Handle the error case
+				throw new Error("Failed to make the employee inactive");
+			}
+		} catch (error) {
+			// Handle any other errors
+			console.error("An error occurred:", error);
+			throw error;
+		}
+	};
 
-    }, []);
+	const openModalEmpUpdate = (data) => {
+		setModalEmpUpdate(data);
+		setModalEmpUpdateConfirm(true);
+	};
 
-    const deleteEmployee = async(data) => {
-        console.log("deleteEmployee...",data);
-        await axios.post("http://localhost:8070/REmployee/addRemovedEmp/",{data}).then(() =>{
-            Swal.fire({
-                title: 'Success!',
-                text: 'Permenantly deleted the Employee Record',
-                icon: 'success',
-                showConfirmButton: false,
-                timer: 1500
-            })
-            console.log("Emp delete modal....");
+	const openModalEmpDelete = (data) => {
+		console.log("delEmp");
+		setModalEmpDelete(data);
+		setModalEmpDeleteConfirm(true);
+	};
 
-            console.log(ModalEmpDelete);
-            const value = axios.post
-            ("http://localhost:8070/employee/deleteEmp",ModalEmpDelete);
-            if(value){
-                Swal.fire({
-                    title: 'Success!',
-                    text: 'Permenantly deleted the Employee Record &  added successfully into the Removed Employee List !!',
-                    icon: 'success',
-                    showConfirmButton: false,
-                    timer: 2000
-                }).then(()=>{
-                    console.log("111111111111");
-                    window.location.reload();
-                })
-            }
-        }).catch((err)=>{
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'Something went wrong!',
-                confirmButtonColor: '#207159',
+    const openModalEmpActivate = (data) => {
+		console.log("delEmp");
+		setModalEmpActive(data);
+		setModalEmpActiveConfirm(true);
+	};
 
-            }).then(() => {
-                window.location.reload();
-            })
-            alert(err.response.data.errorCode)
-        })
-    }
+	return (
+		<div className="container pt-2">
+			<div className="page-component-body">
+				<Header></Header>
 
+				<div className="table-emp" style={{ width: "1180px" }}>
+					<div class="row table-head mt-4 mb-5">
+						<div class="col">
+							<h3 className="float-left">
+								List of All Employees
+							</h3>
+						</div>
+						<a href="/add-new-employee" class="float-right">
+							<button
+								class="btn btn-ok white"
+								style={{ marginRight: "25px" }}>
+								+ &nbsp; Add Employee
+							</button>
+						</a>
+						<a
+							href="/active-employee-list"
+							class="float-right">
+							<button
+								class="btn btn-ok white"
+								style={{ marginRight: "25px" }}>
+								Active Employees
+							</button>
+						</a>
+						<a
+							href="/inactive-employee-list"
+							class="float-right">
+							<button class="btn btn-ok white">
+								Inactive Employees
+							</button>
+						</a>
+					</div>
 
-    const openModalEmpUpdate = (data) => {
-        setModalEmpUpdate(data);
-        setModalEmpUpdateConfirm(true);
-    }
+					<table class="table table-hover">
+						<thead class="thead-dark">
+							<tr>
+								<th
+									class="text-center"
+									style={{ width: "105px" }}>
+									First Name
+								</th>
+								<th
+									class="text-center"
+									style={{ width: "105px" }}>
+									Last Name
+								</th>
+								<th
+									class="text-center"
+									style={{ width: "115px" }}>
+									Address
+								</th>
+								<th
+									class="text-center"
+									style={{ width: "115px" }}>
+									Email Address
+								</th>
+								<th
+									class="text-center"
+									style={{ width: "120px" }}>
+									Phone Number
+								</th>
+								<th
+									class="text-center"
+									style={{ width: "115px" }}>
+									NIC
+								</th>
+								<th
+									class="text-center"
+									style={{ width: "95px" }}>
+									Designation
+								</th>
+								<th
+									class="text-center"
+									style={{ width: "155px" }}>
+									Action
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							{employees.map((employee) => {
+								return (
+									<tr>
+										<td class="text-center">
+											{employee.firstName}
+										</td>
+										<td class="text-center">
+											{employee.lastName}
+										</td>
+										<td class="text-center">
+											{employee.address}
+										</td>
+										<td class="text-center">
+											{employee.emailAddress}
+										</td>
+										<td class="text-center">
+											{employee.phoneNumber}
+										</td>
+										<td class="text-center">
+											{employee.nic}
+										</td>
+										<td class="text-center">
+											{employee.designation}
+										</td>
+										<td class="text-center">
+											<button
+												class="btn btn-warning btn-sm"
+												style={{
+													marginRight: "4px",
+												}}
+												onClick={() =>
+													openModalEmpUpdate(
+														employee,
+													)
+												}>
+												Update
+											</button>
+											{employee.isActive ? ( // Check if employee.isActive is true
+												<button
+													onClick={() =>
+														openModalEmpDelete(
+															employee,
+														)
+													}
+													class="btn btn-danger btn-sm">
+													Inactive
+												</button>
+											) : (
+												// If employee.isActive is false
+												<button
+													onClick={() =>
+														openModalEmpActivate(
+															employee,
+														)
+													}
+													class="btn btn-success btn-sm">
+													Active
+												</button>
+											)}
+											{/* <button
+												onClick={() =>
+													openModalEmpDelete(
+														employee,
+													)
+												}
+												class="btn btn-danger btn-sm">
+												Inactive
+											</button> */}
+										</td>
+									</tr>
+								);
+							})}
+						</tbody>
+					</table>
+				</div>
 
-    const openModalEmpDelete = (data) => {
-        console.log("delEmp")
-        setModalEmpDelete(data);
-        setModalEmpDeleteConfirm(true);
-    }
-  
+				{/* modal for update employee details */}
+				<Modal
+					show={ModalEmpUpdateConfirm}
+					onHide={() => setModalEmpUpdateConfirm(false)}
+					size="lg"
+					aria-labelledby="contained-modal-title-vcenter"
+					centered>
+					<UpdateEmployee
+						data={ModalEmpUpdate}
+						onHide={() => setModalEmpUpdate(false)}
+					/>
+				</Modal>
 
+				{/* modal for delete employee details */}
+				<Modal
+					show={ModalEmpDeleteConfirm}
+					onHide={() => setModalEmpDeleteConfirm(false)}
+					size="md"
+					aria-labelledby="contained-modal-title-vcenter"
+					centered>
+					<Modal.Header closeButton>
+						<Modal.Title>Confirm Deletion</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						<p>
+							Would you like to remove this employee's
+							details ?
+						</p>
+					</Modal.Body>
+					<Modal.Footer>
+						<div className="row">
+							<div className="col -6">
+								<button
+									type="submit"
+									className="btn btn-delete"
+									onClick={() => {
+										deleteEmployee(ModalEmpDelete);
+									}}>
+									Confirm
+								</button>
+							</div>
+							<div
+								className="col-6 text-right"
+								onClick={() =>
+									setModalEmpDeleteConfirm(false)
+								}>
+								<button
+									type="reset"
+									className="btn btn-reset">
+									cancel
+								</button>
+							</div>
+						</div>
+					</Modal.Footer>
+				</Modal>
 
-    return (
-        <div className='container pt-2'>
-
-        <div className="page-component-body">
-            <Header></Header>
-
-
-            <div className="table-emp">
-                <div class="row table-head mt-3">
-                    <div class="col">
-                        <h3 className="float-left ">List of current employees</h3>
-                    </div>
-                    <a href="/addEmp" class="float-right">
-                        <button class="btn btn-ok white">
-                            Add Employee
-                        </button>
-                    </a>
-                    <a href="/REmp" class="float-right">
-                        <button class="btn btn-ok white">
-                            Resigned Employees
-                        </button>
-                    </a>
-                   
-                </div>
-            
-
-                <table class="table table-hover">
-                    <thead class="thead-dark">
-                        <tr>
-                            <th class="text-center">EmpName</th>
-                            <th class="text-center">Email</th>
-                            <th class="text-center">Phone</th>
-                            <th class="text-center">NIC</th>
-                            <th class="text-right">Designation</th>
-                            <th class="text-right">Action</th>
-
-                        </tr>
-
-                    </thead>
-                    <tbody>
-                        {employees.map((employee) => {
-
-                            return (
-                                <tr>
-                                    <td class="text-center">{employee.Name}</td>
-                                    <td class="text-center">{employee.Email}</td>
-                                    <td class="text-center">{employee.Phone}</td>
-                                    <td class="text-center">{employee.NIC}</td>
-                                    <td class="text-right">{employee.Designation}</td>
-                                    <td class="text-center">
-                                        <button
-                                             class="btn btn-light btn-sm"
-                                             onClick={() => openModalEmpUpdate(employee)}
-                                        >
-                                            Update
-                                        </button>
-                                        <button
-                                            onClick={() => openModalEmpDelete(employee)}
-                                            class="btn btn-danger btn-sm"
-                                        >
-                                            Delete
-                                        </button>
-                                    </td>
-                                </tr>
-                            );
-
-
-                        })}
-                    </tbody>
-                </table>
-            </div>
-
-            {/* modal for update employee details */}
-            <Modal
-                show={ModalEmpUpdateConfirm}
-                onHide={() => setModalEmpUpdateConfirm(false)}
-                size="lg"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
-                >
-            <UpdateEmployee
-                data={ModalEmpUpdate}
-                onHide={() => setModalEmpUpdate(false)}
-                />
-            </Modal>
-
-            {/* modal for delete employee details */}
-            <Modal
-                show={ModalEmpDeleteConfirm}
-                onHide={() => setModalEmpDeleteConfirm(false)}
-                size="md"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
-                >
-            <Modal.Header closeButton>
-                <Modal.Title>Confirm Deletion</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <p>Would you like to remove this employee's details ?</p>
-            </Modal.Body>
-            <Modal.Footer>
-                <div className="row">
-                    <div className="col -6">
-                        <button type="submit" className="btn btn-delete" onClick={() => {deleteEmployee(ModalEmpDelete);}}>
-                            Confirm
-                        </button>
-                    </div>
-                    <div className="col-6 text-right" onClick={() => setModalEmpDeleteConfirm(false)}>
-                        <button type="reset" className="btn btn-reset">
-                            cancel
-                        </button>
-                    </div>
-                </div>
-            </Modal.Footer>
-            </Modal>
-           
-        </div>
-        </div>
-    )
+                	{/* modal for active employee details */}
+				<Modal
+					show={ModalEmpActiveConfirm}
+					onHide={() => setModalEmpActiveConfirm(false)}
+					size="md"
+					aria-labelledby="contained-modal-title-vcenter"
+					centered>
+					<Modal.Header closeButton>
+						<Modal.Title>Confirm Activation</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						<p>
+							Would you like to active this employee's
+							details ?
+						</p>
+					</Modal.Body>
+					<Modal.Footer>
+						<div className="row">
+							<div className="col -6">
+								<button
+									type="submit"
+									className="btn btn-delete"
+									onClick={() => {
+										restoreEmployee(ModalEmpActive);
+									}}>
+									Confirm
+								</button>
+							</div>
+							<div
+								className="col-6 text-right"
+								onClick={() =>
+									setModalEmpActiveConfirm(false)
+								}>
+								<button
+									type="reset"
+									className="btn btn-reset">
+									cancel
+								</button>
+							</div>
+						</div>
+					</Modal.Footer>
+				</Modal>
+			</div>
+		</div>
+	);
 }
-
