1569c364576038b8ef193a3fac93a86d694ca95f
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2023-10-01 23:39:22 +0530

    create routes for employee management

--- frontend/src/services/util/employee/index.js
+++ frontend/src/services/util/employee/index.js
@@ -1,13 +1,86 @@
-import { axiosInstance } from '../../api';
-const BASE_PATH = '/employee';
-
+import { axiosInstance } from "../../api";
+const BASE_PATH = "/employee";
 
 //signIn API Call
-export let viewEmployees = async (email, password) => {
-    try {
-        let value = await axiosInstance.get(BASE_PATH+ '/view-all');
-        return value;
-    } catch (error) {
-        return error
-    }
-}
+export let viewEmployees = async () => {
+	try {
+		let value = await axiosInstance.get(BASE_PATH + "/view-all");
+		return value;
+	} catch (error) {
+		return error;
+	}
+};
+
+export let viewAllInactiveEmployees = async () => {
+	try {
+		let value = await axiosInstance.get(
+			BASE_PATH + "/get-inactive-employees",
+		);
+		return value;
+	} catch (error) {
+		return error;
+	}
+};
+
+export let inactiveEmployee = async (data) => {
+	try {
+		let value = await axiosInstance.put(
+			BASE_PATH + `/inactive_employee`,
+			{
+				data: data,
+			},
+		);
+		return value;
+	} catch (error) {
+		return error;
+	}
+};
+
+export let activeEmployee = async (data) => {
+	try {
+		let value = await axiosInstance.put(
+			BASE_PATH + `/active_employee`,
+			{
+				data: data,
+			},
+		);
+		return value;
+	} catch (error) {
+		return error;
+	}
+};
+
+export let addEmployees = async (employeeData) => {
+	try {
+		const response = await axiosInstance.post(
+			BASE_PATH + "/save_employee_details",
+			employeeData,
+		);
+		return response.data; // Return the response data from the server
+	} catch (error) {
+		throw error;
+	}
+};
+
+export let viewAllActiveEmployees = async () => {
+	try {
+		let value = await axiosInstance.get(
+			BASE_PATH + "/get-active-employees",
+		);
+		return value;
+	} catch (error) {
+		return error;
+	}
+};
+
+export let updateEmployees = async (data) => {
+	try {
+		let value = await axiosInstance.put(
+			BASE_PATH + `/update_employee/`,
+			data,
+		);
+		return value.data;
+	} catch (error) {
+		return error;
+	}
+};
