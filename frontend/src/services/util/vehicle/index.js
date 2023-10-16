2bd76ce989abfb4efb36f499124d7b29609c27e7
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2023-10-16 21:04:25 +0530

    implement delete vehicle function

--- frontend/src/services/util/vehicle/index.js
+++ frontend/src/services/util/vehicle/index.js
@@ -1,7 +1,6 @@
 import { axiosInstance } from "../../api";
 const BASE_PATH = "/vehicle";
 
-
 export let addVehicle = async (vehicleData) => {
 	try {
 		const response = await axiosInstance.post(
@@ -15,25 +14,44 @@ export let addVehicle = async (vehicleData) => {
 };
 
 export let viewVehicle = async () => {
-    try {
-        const response = await axiosInstance.get(BASE_PATH + "/view-all");
-        return response.data; // Return the response data from the server
-    } catch (error) {
-        throw error;
-    }
+	try {
+		const response = await axiosInstance.get(BASE_PATH + "/view-all");
+		return response.data; // Return the response data from the server
+	} catch (error) {
+		throw error;
+	}
 };
 
 export let updateVehicle = async (vehicleID, vehicleData) => {
-    try {
-        const response = await axiosInstance.put(
-            BASE_PATH + "/updateVehicle",
-            {
-                _id: vehicleID,
-                ...vehicleData
-            }
-        );
-        return response.data; // Return the response data from the server
-    } catch (error) {
-        throw error;
-    }
+	try {
+		const response = await axiosInstance.put(
+			BASE_PATH + "/updateVehicle",
+			{
+				_id: vehicleID,
+				...vehicleData,
+			},
+		);
+		return response.data; // Return the response data from the server
+	} catch (error) {
+		throw error;
+	}
+};
+
+export const deleteVehicleService = async (vehicleId) => {
+	try {
+		const response = await fetch("/deleteVehicle", {
+			method: "PUT",
+			body: JSON.stringify({ _id: vehicleId }),
+		});
+
+		if (response.ok) {
+			const data = await response.json();
+
+			return data;
+		} else {
+			throw new Error("Failed to delete the vehicle");
+		}
+	} catch (error) {
+		throw error;
+	}
 };
